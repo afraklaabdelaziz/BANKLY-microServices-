3facf23938dd4c5cfd75aced805c6839830e0aaa
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2023-02-02 09:22:19 +0100

    crée micro-service gateway et ajoutée entity user

---
+++ gateway-service/src/main/java/com/bankly/gatewayservice/entities/UserApp.java
@@ -0,0 +1,105 @@
+package com.bankly.gatewayservice.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class UserApp implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String lastName;
+    private String firstName;
+    @Column(unique = true)
+    private String cin;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phone;
+
+    private String password;
+
+    public UserApp(String lastName, String firstName, String cin, String email, String phone, String password) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.cin = cin;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public UserApp() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserApp{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", cin='" + cin + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
