6212111e22dd1795b3129ea62f123c76c75b6063
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2023-02-03 12:21:11 +0100

    add apis add,wallet-client,wallet-all of wallet

--- wallet-service/src/main/java/com/bankly/walletservice/controller/WalletController.java
+++ wallet-service/src/main/java/com/bankly/walletservice/controller/WalletController.java
@@ -1,4 +1,31 @@
 package com.bankly.walletservice.controller;
 
+import com.bankly.walletservice.dto.ResponceDto;
+import com.bankly.walletservice.entities.Wallet;
+import com.bankly.walletservice.services.IWalletService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/wallet")
 public class WalletController {
+    private IWalletService walletService;
+
+    public WalletController(IWalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @PostMapping("add-wallet")
+    public ResponceDto addWallet(@RequestBody Wallet wallet){
+        return walletService.addWallet(wallet);
+    }
+
+    @GetMapping("/wallet-client/{cin}")
+    public ResponceDto findWalletOfClient(@PathVariable String cin){
+        return walletService.findWalletByCinClient(cin);
+    }
+
+    @GetMapping("all-wallet")
+    public ResponceDto findAllWallet(){
+        return walletService.findAllWallet();
+    }
 }
