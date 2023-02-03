d57031301c86dffbc2e48c5d6a0eddd87516415a
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2023-02-03 11:28:23 +0100

    change

--- gateway-service/src/main/java/com/bankly/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/bankly/gatewayservice/GatewayServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
