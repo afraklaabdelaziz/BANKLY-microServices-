37d2df4523c35b814a5ee6686831c662b0dfdf2f
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2023-02-10 12:26:22 +0100

    ajouter class user dto

---
+++ gateway-service/src/main/java/com/bankly/gatewayservice/dto/UserAppDto.java
@@ -0,0 +1,87 @@
+package com.bankly.gatewayservice.dto;
+
+import java.io.Serializable;
+
+public class UserAppDto implements Serializable {
+    private String lastName;
+    private String firstName;
+    private String cin;
+
+    private String email;
+
+    private String phone;
+
+    private String password;
+
+    public UserAppDto(String lastName, String firstName, String cin, String email, String phone, String password) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.cin = cin;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public UserAppDto() {
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserAppDto{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", cin='" + cin + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
